9f81edcb7ec9c76aa26c95590db8aa0913ebedb0
Jorge Mg <44137164+itsjorgemg@users.noreply.github.com>
2019-10-07 19:05:13 +0200

    Agregar archivos de proyecto.

---
+++ VJ01-contra/Scene.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <cmath>
+#include <glm/gtc/matrix_transform.hpp>
+#include "Scene.h"
+#include "Game.h"
+
+
+#define SCREEN_X 0
+#define SCREEN_Y 0
+
+#define INIT_PLAYER_X_TILES 1
+#define INIT_PLAYER_Y_TILES 2
+
+
+Scene::Scene()
+{
+	map = NULL;
+	player = NULL;
+}
+
+Scene::~Scene()
+{
+	if(map != NULL)
+		delete map;
+	if(player != NULL)
+		delete player;
+}
+
+
+void Scene::init()
+{
+	initShaders();
+	map = TileMap::createTileMap("levels/level01.txt", glm::vec2(SCREEN_X, SCREEN_Y), texProgram);
+	player = new Player();
+	player->init(glm::ivec2(SCREEN_X, SCREEN_Y), texProgram);
+	player->setPosition(glm::vec2(INIT_PLAYER_X_TILES * map->getTileSize(), INIT_PLAYER_Y_TILES * map->getTileSize()));
+	player->setTileMap(map);
+	//projection = glm::ortho(0.f, float(SCREEN_WIDTH - 1), float(SCREEN_HEIGHT - 1), 0.f);
+	projection = glm::ortho(0.f, 32*8.0f, 32*7.0f, 0.f);
+	//projection = glm::scale(projection, glm::vec3(2.0f));
+	currentTime = 0.0f;
+}
+
+void Scene::update(int deltaTime)
+{
+	currentTime += deltaTime;
+	player->update(deltaTime);
+	if (Game::instance().getKey('.')) {
+		projection = glm::translate(projection, glm::vec3(-1.0f, 0.0f, 0.0f));
+	} else if (Game::instance().getKey(',')) {
+		projection = glm::translate(projection, glm::vec3(1.0f, 0.0f, 0.0f));
+	}
+	//projection = glm::translate(projection, glm::vec3(-deltaTime/32.0f, 0.0f, 0.0f));
+}
+
+void Scene::render()
+{
+	glm::mat4 modelview;
+
+	texProgram.use();
+	texProgram.setUniformMatrix4f("projection", projection);
+	texProgram.setUniform4f("color", 1.0f, 1.0f, 1.0f, 1.0f);
+	modelview = glm::mat4(1.0f);
+	texProgram.setUniformMatrix4f("modelview", modelview);
+	texProgram.setUniform2f("texCoordDispl", 0.f, 0.f);
+	map->render();
+	player->render();
+}
+
+void Scene::initShaders()
+{
+	Shader vShader, fShader;
+
+	vShader.initFromFile(VERTEX_SHADER, "shaders/texture.vert");
+	if(!vShader.isCompiled())
+	{
+		cout << "Vertex Shader Error" << endl;
+		cout << "" << vShader.log() << endl << endl;
+	}
+	fShader.initFromFile(FRAGMENT_SHADER, "shaders/texture.frag");
+	if(!fShader.isCompiled())
+	{
+		cout << "Fragment Shader Error" << endl;
+		cout << "" << fShader.log() << endl << endl;
+	}
+	texProgram.init();
+	texProgram.addShader(vShader);
+	texProgram.addShader(fShader);
+	texProgram.link();
+	if(!texProgram.isLinked())
+	{
+		cout << "Shader Linking Error" << endl;
+		cout << "" << texProgram.log() << endl << endl;
+	}
+	texProgram.bindFragmentOutput("outColor");
+	vShader.free();
+	fShader.free();
+}
+
+
+
